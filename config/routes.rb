8284e0d6600334064080a9fb2c0a49377c0be192
Franz Schurmann <schuermann.franz@gmail.com>
2023-11-17 14:18:18 +0100

    added the min required functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create, :destroy]
+  end
+  # get '/lists', to: 'lists#index'
 
+  # get '/lists/:id', to: 'lists#show'
+
+  # get '/lists/new', to: 'lists#new', as: :new_list
+  # post '/lists', to: 'lists#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
